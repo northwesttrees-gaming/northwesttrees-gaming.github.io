3d4787d6945a6b52bb84341044751e0ee8951659
NorthWestTrees <northwesttreesgaming@gmail.com>
2021-08-23 13:39:31 -0700

    Updated Site

--- files/js/menu.js
+++ files/js/menu.js
@@ -1,16 +1,60 @@
 function mainMenuButton() {
-  var x = document.getElementById("main-menu-button");
+  var x = document.getElementById("main-menu-button-0");
     if (x.style.display == "none") {
       x.style.display = "block";
       } else {
       x.style.display = "none";
+	};
+  var y = document.getElementById("main-menu-button-1");
+    if (y.style.display == "none") {
+      y.style.display = "inline";
+      } else {
+      y.style.display = "none";
+	};
+  var z = document.getElementById("main-menu-button-2");
+    if (z.style.display == "none") {
+      z.style.display = "inline";
+      } else {
+      z.style.display = "none";
 	}
 }
 function modMenuButton() {
-  var x = document.getElementById("mod-menu-button");
+  var x = document.getElementById("mod-menu-button-0");
     if (x.style.display == "none") {
       x.style.display = "block";
       } else {
       x.style.display = "none";
+	};
+  var y = document.getElementById("mod-menu-button-1");
+    if (y.style.display == "none") {
+      y.style.display = "inline";
+      } else {
+      y.style.display = "none";
+	};
+  var z = document.getElementById("mod-menu-button-2");
+    if (z.style.display == "none") {
+      z.style.display = "inline";
+      } else {
+      z.style.display = "none";
+	}
+}
+function asideMenuButton() {
+  var x = document.getElementById("aside-menu-button-0");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+      } else {
+      x.style.display = "none";
+	};
+  var y = document.getElementById("aside-menu-button-1");
+    if (y.style.display == "none") {
+      y.style.display = "inline";
+      } else {
+      y.style.display = "none";
+	};
+  var z = document.getElementById("aside-menu-button-2");
+    if (z.style.display == "none") {
+      z.style.display = "inline";
+      } else {
+      z.style.display = "none";
 	}
 }
