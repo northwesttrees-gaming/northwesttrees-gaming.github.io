b63e480fede1a4f077c43661ff87a0488747b13f
NorthWestTrees <northwesttreesgaming@gmail.com>
2021-08-22 22:35:52 -0700

    test

---
+++ files/js/menu.js
@@ -0,0 +1,16 @@
+function mainMenuButton() {
+  var x = document.getElementById("main-menu-button");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+      } else {
+      x.style.display = "none";
+	}
+}
+function modMenuButton() {
+  var x = document.getElementById("mod-menu-button");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+      } else {
+      x.style.display = "none";
+	}
+}
